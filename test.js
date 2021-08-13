18b29ee69ddcb80b874847542d9d30ae2ef74226
rain song <e1900594321@gmail.com>
2021-08-13 19:01:34 +0800

    1

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ import { HttpJsonRpcConnector, LotusClient } from 'filecoin.js';
 (async () => {
 
     const connector = new HttpJsonRpcConnector({ url:'http://127.0.0.1:1234/rpc/v1', token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJBbGxvdyI6WyJyZWFkIiwid3JpdGUiLCJzaWduIiwiYWRtaW4iXX0.q8bmFopRibNBkyc_0pI8ze11d9EdDTmyjxwfcbfTFbE' });
-    const jsonRpcProvider = new LotusClient(httpConnector);
+    const jsonRpcProvider = new LotusClient(connector);
     const version = await jsonRpcProvider.common.version();
     console.log(version);
 

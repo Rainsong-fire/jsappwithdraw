1d43d79e6444683c146b8ac1457599e6fb167399
rain song <e1900594321@gmail.com>
2021-08-13 19:14:16 +0800

    1

--- httpwithdraw.js
+++ httpwithdraw.js
@@ -6,5 +6,8 @@ import {HttpJsonRpcConnector, LotusClient, LotusWalletProvider} from 'filecoin.j
     const connector = new HttpJsonRpcConnector({ url:'http://127.0.0.1:1234/rpc/v1', token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJBbGxvdyI6WyJyZWFkIiwid3JpdGUiLCJzaWduIiwiYWRtaW4iXX0.q8bmFopRibNBkyc_0pI8ze11d9EdDTmyjxwfcbfTFbE' });
     const lotusClient = new LotusClient(connector);
     const walletLotusHttp = new LotusWalletProvider(lotusClient);
-    console.log('11111ok')
+    console.log('11111ok');
+    const nonce = new walletLotusHttp.getNonce('t1npbdebqhmt6vo6asvrg64knpxjiftacpv4ccvja');
+    console.log(nonce);
+
 })().then().catch();

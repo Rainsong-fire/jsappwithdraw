a67d66b64bcd2988eb379da3d127d8e9fe3ea1e5
rain song <e1900594321@gmail.com>
2021-08-15 10:15:19 +0800

    1

--- filecoinjssend.js
+++ filecoinjssend.js
@@ -1,8 +1,8 @@
-import {HttpJsonRpcConnector, LotusClient, LotusWalletProvider} from 'filecoin.js';
-
+import {HttpJsonRpcConnector, LotusClient, LotusWalletProvider } from 'filecoin.js';
+import BigNumber from "bignumber.js";
 
 (async () => {
-    let addr;
+
     const connector = new HttpJsonRpcConnector({ url:'http://127.0.0.1:1234/rpc/v1', token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJBbGxvdyI6WyJyZWFkIiwid3JpdGUiLCJzaWduIiwiYWRtaW4iXX0.q8bmFopRibNBkyc_0pI8ze11d9EdDTmyjxwfcbfTFbE' });
     const lotusClient = new LotusClient(connector);
     const walletLotusHttp = new LotusWalletProvider(lotusClient);
